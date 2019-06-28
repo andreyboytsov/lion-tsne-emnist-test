1205000fe64928b2dce1a2bc90faabde0dc56189
andreyboytsov <andrey.m.boytsov@gmail.com>
2019-06-29 00:35:01 +0200

    Moving towards figure 2

--- mnist-experiments/table-generators/tab-cluster-attribution-test.py
+++ mnist-experiments/table-generators/tab-cluster-attribution-test.py
@@ -31,29 +31,29 @@ nn_results_file = exp_cluter_attr_test_NN.generate_cluster_results_filename(para
 with open(nn_results_file, 'rb') as f:
         nn_method_results, nn_models_orig, nn_method_list = pickle.load(f)
 
-accuracy_multiquadric = all_RBF_IDW_LION_results["RBF-multiquadric"]['Accuracy']
-accuracy_gaussian = all_RBF_IDW_LION_results["RBF-gaussian"]['Accuracy']
-accuracy_linear = all_RBF_IDW_LION_results["RBF-linear"]['Accuracy']
-accuracy_cubic = all_RBF_IDW_LION_results["RBF-cubic"]['Accuracy']
-accuracy_quintic = all_RBF_IDW_LION_results["RBF-quintic"]['Accuracy']
-accuracy_inverse = all_RBF_IDW_LION_results["RBF-inverse"]['Accuracy']
-accuracy_thin_plate = all_RBF_IDW_LION_results["RBF-thin-plate"]['Accuracy']
+accuracy_multiquadric = np.mean(all_RBF_IDW_LION_results["RBF-multiquadric"]['Accuracy'])
+accuracy_gaussian = np.mean(all_RBF_IDW_LION_results["RBF-gaussian"]['Accuracy'])
+accuracy_linear = np.mean(all_RBF_IDW_LION_results["RBF-linear"]['Accuracy'])
+accuracy_cubic = np.mean(all_RBF_IDW_LION_results["RBF-cubic"]['Accuracy'])
+accuracy_quintic = np.mean(all_RBF_IDW_LION_results["RBF-quintic"]['Accuracy'])
+accuracy_inverse = np.mean(all_RBF_IDW_LION_results["RBF-inverse"]['Accuracy'])
+accuracy_thin_plate = np.mean(all_RBF_IDW_LION_results["RBF-thin-plate"]['Accuracy'])
 
-kl_multiquadric = all_RBF_IDW_LION_results["RBF-multiquadric"]["KL-Divergence"]
-kl_gaussian = all_RBF_IDW_LION_results["RBF-gaussian"]["KL-Divergence"]
-kl_linear = all_RBF_IDW_LION_results["RBF-linear"]["KL-Divergence"]
-kl_cubic = all_RBF_IDW_LION_results["RBF-cubic"]["KL-Divergence"]
-kl_quintic = all_RBF_IDW_LION_results["RBF-quintic"]["KL-Divergence"]
-kl_inverse = all_RBF_IDW_LION_results["RBF-inverse"]["KL-Divergence"]
-kl_thin_plate = all_RBF_IDW_LION_results["RBF-thin-plate"]["KL-Divergence"]
+kl_multiquadric = np.mean(all_RBF_IDW_LION_results["RBF-multiquadric"]["KL-Divergence"])
+kl_gaussian = np.mean(all_RBF_IDW_LION_results["RBF-gaussian"]["KL-Divergence"])
+kl_linear = np.mean(all_RBF_IDW_LION_results["RBF-linear"]["KL-Divergence"])
+kl_cubic = np.mean(all_RBF_IDW_LION_results["RBF-cubic"]["KL-Divergence"])
+kl_quintic = np.mean(all_RBF_IDW_LION_results["RBF-quintic"]["KL-Divergence"])
+kl_inverse = np.mean(all_RBF_IDW_LION_results["RBF-inverse"]["KL-Divergence"])
+kl_thin_plate = np.mean(all_RBF_IDW_LION_results["RBF-thin-plate"]["KL-Divergence"])
 
-dist_multiquadric = all_RBF_IDW_LION_results["RBF-multiquadric"]["DistancePercentile"]
-dist_gaussian = all_RBF_IDW_LION_results["RBF-gaussian"]["DistancePercentile"]
-dist_linear = all_RBF_IDW_LION_results["RBF-linear"]["DistancePercentile"]
-dist_cubic = all_RBF_IDW_LION_results["RBF-cubic"]["DistancePercentile"]
-dist_quintic = all_RBF_IDW_LION_results["RBF-quintic"]["DistancePercentile"]
-dist_inverse = all_RBF_IDW_LION_results["RBF-inverse"]["DistancePercentile"]
-dist_thin_plate = all_RBF_IDW_LION_results["RBF-thin-plate"]["DistancePercentile"]
+dist_multiquadric = np.mean(all_RBF_IDW_LION_results["RBF-multiquadric"]["DistancePercentile"])
+dist_gaussian = np.mean(all_RBF_IDW_LION_results["RBF-gaussian"]["DistancePercentile"])
+dist_linear = np.mean(all_RBF_IDW_LION_results["RBF-linear"]["DistancePercentile"])
+dist_cubic = np.mean(all_RBF_IDW_LION_results["RBF-cubic"]["DistancePercentile"])
+dist_quintic = np.mean(all_RBF_IDW_LION_results["RBF-quintic"]["DistancePercentile"])
+dist_inverse = np.mean(all_RBF_IDW_LION_results["RBF-inverse"]["DistancePercentile"])
+dist_thin_plate = np.mean(all_RBF_IDW_LION_results["RBF-thin-plate"]["DistancePercentile"])
 
 rbf_method_list = ['RBF - Multiquadric', 'RBF - Gaussian',
         'RBF - Inverse Multiquadric', 'RBF - Linear', 'RBF - Cubic', 'RBF - Quintic',
@@ -66,20 +66,43 @@ rbf_distance_percentiles = [dist_multiquadric, dist_gaussian, dist_inverse, dist
                       dist_thin_plate]
 rbf_avg_kl = [kl_multiquadric, kl_gaussian, kl_inverse, kl_linear, kl_cubic, kl_quintic, kl_thin_plate]
 
-
 # =============================================================================================================
 keys_copy = all_RBF_IDW_LION_results.keys()
 keys_copy -= {"IDW-1","IDW-10","IDW-20","IDW-40"}
 idw_optimal_name = [i for i in keys_copy if i.startswith("IDW")][0]
 print(idw_optimal_name)
-accuracy_idw1 = all_RBF_IDW_LION_results['IDW-1']['Accuracy']
-accuracy_idw10 = all_RBF_IDW_LION_results['IDW-10']['Accuracy']
-accuracy_idw20 = all_RBF_IDW_LION_results['IDW-20']['Accuracy']
-accuracy_idw40 = all_RBF_IDW_LION_results['IDW-40']['Accuracy']
-accuracy_idw_optimal = all_RBF_IDW_LION_results[idw_optimal_name]['Accuracy']
+accuracy_idw1 = np.mean(all_RBF_IDW_LION_results['IDW-1']['Accuracy'])
+accuracy_idw10 = np.mean(all_RBF_IDW_LION_results['IDW-10']['Accuracy'])
+accuracy_idw20 = np.mean(all_RBF_IDW_LION_results['IDW-20']['Accuracy'])
+accuracy_idw40 = np.mean(all_RBF_IDW_LION_results['IDW-40']['Accuracy'])
+accuracy_idw_optimal = np.mean(all_RBF_IDW_LION_results[idw_optimal_name]['Accuracy'])
+
+kl_idw1 = np.mean(all_RBF_IDW_LION_results['IDW-1']['KL-Divergence'])
+kl_idw10 = np.mean(all_RBF_IDW_LION_results['IDW-10']['KL-Divergence'])
+kl_idw20 = np.mean(all_RBF_IDW_LION_results['IDW-20']['KL-Divergence'])
+kl_idw40 = np.mean(all_RBF_IDW_LION_results['IDW-40']['KL-Divergence'])
+kl_idw_optimal = np.mean(all_RBF_IDW_LION_results[idw_optimal_name]['KL-Divergence'])
+
+dist_idw1 = np.mean(all_RBF_IDW_LION_results['IDW-1']['DistancePercentile'])
+dist_idw10 = np.mean(all_RBF_IDW_LION_results['IDW-10']['DistancePercentile'])
+dist_idw20 = np.mean(all_RBF_IDW_LION_results['IDW-20']['DistancePercentile'])
+dist_idw40 = np.mean(all_RBF_IDW_LION_results['IDW-40']['DistancePercentile'])
+dist_idw_optimal = np.mean(all_RBF_IDW_LION_results[idw_optimal_name]['DistancePercentile'])
 
 idw_method_list = ["IDW - Power 1","IDW - Power 10", "IDW - Power 20",
     "IDW - Power "+idw_optimal_name[-4:], "IDW - Power 40"]
+
+idw_accuracy = [accuracy_idw1, accuracy_idw10, accuracy_idw20, accuracy_idw40,
+                accuracy_idw_optimal]
+idw_distance_percentiles = [dist_idw1, dist_idw10, dist_idw20, dist_idw40, dist_idw_optimal]
+idw_avg_kl = [kl_idw1, kl_idw10, kl_idw20, kl_idw40, kl_idw_optimal]
+
+
+for i in range(len(rbf_method_list)):
+    print(rbf_method_list[i], rbf_accuracy[i], rbf_distance_percentiles[i], rbf_avg_kl[i])
+for i in range(len(idw_method_list)):
+    print(idw_method_list[i], idw_accuracy[i], idw_distance_percentiles[i], idw_avg_kl[i])
+exit(-1)
 
 lion90_name = [i for i in all_RBF_IDW_LION_results.keys() if i.startswith('LION-90')][0]
 accuracy__lion90 = all_RBF_IDW_LION_results[lion90_name]['Accuracy']
@@ -114,9 +137,6 @@ with open(gd_results_file, 'rb') as f:
      picked_neighbors_y_gd_variance_recalc_early_exagg_transformed, covered_samples) = pickle.load(f)
 
 print("DATA LOADED")
-
-idw_method_results = [picked_neighbors_y_idw1, picked_neighbors_y_idw10, picked_neighbors_y_idw20,
-                      picked_neighbors_y_idw_optimal, picked_neighbors_y_idw40]
 
 gd_method_list = [r'Closest $Y_{init}$',
               r'Random $Y_{init}$',
